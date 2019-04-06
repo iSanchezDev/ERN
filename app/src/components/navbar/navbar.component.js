f655dedf3ddb26dca13dc20da20f7d87926b96f6
Iván Sánchez <isancheznetwork@gmail.com>
2019-04-06 13:33:38 +0200

    refactor(app): title

--- app/src/components/navbar/navbar.component.js
+++ app/src/components/navbar/navbar.component.js
@@ -8,7 +8,7 @@ class NavBarComponent extends Component {
     return (
       <Row span={24} className={'wp-nav-bar'}>
         <Col xs={16} sm={8} md={10}>
-          <h3><b>MERN</b> <span className='wp-sub-title'>Fetching Cities</span></h3>
+          <h3><b>ERN</b> <span className='wp-sub-title'>Fetching Cities</span></h3>
         </Col>
         <Col xs={8} sm={16} md={14}>
           <Avatar icon="user" className={'wp-nav-bar-avatar'}/>
